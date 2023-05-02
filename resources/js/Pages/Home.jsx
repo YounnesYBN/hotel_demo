7338e62cae5b4edd5cf39b1e15ad6c8102f63b68
YounnesYBN <younnesYBN@outlook.com>
2023-05-02 13:29:43 +0100

    first commit

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/header"
+
+export default function Home(second) {
+    
+    return (
+        <div>
+            <Header />
+            <h1>home</h1>
+        </div>
+    )
+}
---
+++ resources/js/Pages/components/header.jsx
@@ -0,0 +1,21 @@
+
+
+export default function Header(){
+
+
+    return (
+        <div style = {
+            {
+                width:"100%",
+                height : "fit-content",
+                border : "1px solid black"
+                
+            }
+        } >
+            <div id="first-con">
+                
+            </div>
+            
+        </div>
+    )
+}
