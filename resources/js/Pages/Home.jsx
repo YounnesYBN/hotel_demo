0af2c6de38023e0f270141cf447adda302881c26
YounnesYBN <younnesYBN@outlook.com>
2023-05-03 23:59:05 +0100

    almost finish still working on map

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,9 +1,14 @@
-import { Backdrop, Button, Card, CardActionArea, CardContent, CardMedia, IconButton, Tab, Tabs, Typography, useMediaQuery } from "@mui/material";
+import { Backdrop, Button, Card, CardContent, CardMedia, Divider, IconButton, Tab, Tabs, Typography, useMediaQuery } from "@mui/material";
 import Header from "./components/header"
 import { Carousel, Image, Grommet } from "grommet";
 import { useState } from "react";
 import TrendingFlatRoundedIcon from '@mui/icons-material/TrendingFlatRounded';
 import { Close } from "@mui/icons-material";
+import LocalPhoneRoundedIcon from '@mui/icons-material/LocalPhoneRounded';
+import MapRoundedIcon from '@mui/icons-material/MapRounded';
+import AttachEmailRoundedIcon from '@mui/icons-material/AttachEmailRounded';
+import GoogleMapReact from 'google-map-react';
+
 
 const SpliteArray = (x) => {
     var result = [];
@@ -20,34 +25,40 @@ export default function Home({ imgs, galeries }) {
     const [valueGallery, setValueGallery] = useState(0);
     const [imgBackDrop, setImgBackDrop] = useState(false);
     const [selectedImg, setSelectedImg] = useState(null);
-    const IsPhone = useMediaQuery('(max-width:600px)');
+    const IsPhone = useMediaQuery('(max-width:767px)');
     const IssmallPhone = useMediaQuery('(max-width:470px)');
     const IsLessThen470 = useMediaQuery('(max-width:459px)');
     const x = useMediaQuery('(min-width:1018px)');
+    const ContactBreakingPoint = useMediaQuery("(max-width:1182px)")
     const isSmallAboutUs = IsPhone ? {
         backgroundColor: "white",
         borderRadius: "5px",
         boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px"
 
     } : {}
-    const RoomsHover = {
-        boxShadow: "rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px",
-        transform: "translateY(-5px)",
 
-
-    }
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-    };
-    const handleChangeGallery = (event, newValue) => {
-        setValueGallery(newValue);
-    };
     const customTheme = {
         carousel: {
             icons: {
                 color: 'white',
             },
         },
+    };
+    const contactChildrens = {
+        minHeight: "33.33%",
+        maxHeight: "fit-Content",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: "10px 30px"
+
+    }
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+    const handleChangeGallery = (event, newValue) => {
+        setValueGallery(newValue);
     };
     const openBackDrop = (url) => {
         setSelectedImg(url);
@@ -57,6 +68,7 @@ export default function Home({ imgs, galeries }) {
         setSelectedImg(null);
         setImgBackDrop(false);
     }
+    
     return (
         <div>
             <Header selected={"HOME"} />
@@ -402,25 +414,79 @@ export default function Home({ imgs, galeries }) {
 
                         }
                     }>
-                        <Typography variant={IsPhone ? "h5" : "h3"}>
-                            About Us
+                        <Typography variant={IsPhone ? "h5" : "h4"}>
+                            We Would Love To Hear From You
                         </Typography>
                         <Button sx={{ height: "fit-content", "&:hover": { textDecoration: "underline" } }} endIcon={<TrendingFlatRoundedIcon />}>see all</Button>
                     </div>
                     <div style={
                         {
                             width: IsPhone ? "100%" : "70%",
-                            padding: IsPhone ? "20px" : null,
-                            lineHeight: "30px",
-                            ...isSmallAboutUs
+                            display: "flex",
+                            flexDirection: ContactBreakingPoint ? "column" : null,
+                            paddingTop: "10px",
+                            height: "fit-content"
 
                         }
                     }>
-                        Chez Imnir has a garden, shared lounge, a terrace and restaurant in Ijjoukak. Boasting room service, this property also provides guests with a barbecue. There is free private parking and the property features paid airport shuttle service.
-                        At the inn, the rooms include a balcony with a mountain view. Featuring a private bathroom with a shower and free toiletries, rooms at Chez Imnir also feature a city view. At the accommodation rooms are fitted with a seating area.
-                        An à la carte, continental or vegetarian breakfast can be enjoyed at the property.
-                        Speaking Arabic, German, English and Spanish at the reception, staff are willing to help at any time of the day.
-                        The nearest airport is Marrakech-Menara Airport, 98 km from Chez Imnir.
+                        <div style={
+                            {
+                                minHeight: ContactBreakingPoint ? "200px" : "300px",
+                                maxHeight: "fit-Content",
+                                width: ContactBreakingPoint ? "100%" : "40%",
+                                backgroundColor: "white",
+                                borderRadius: ContactBreakingPoint ? "5px 5px 0px 0px " : "5px 0 0 5px",
+                            }
+                        }>
+                            {/* //info : phone adress email */}
+                            <div style={{ ...contactChildrens, }}>
+                                <div>
+                                    {/* info phone */}
+                                    <h4 style={{ color: "#606060", fontWeight: "normal" }}>Telephone Number</h4>
+                                    <span style={{ color: "black" }}>0612345789</span>
+                                </div>
+                                <a href="#">
+                                    <IconButton>
+                                        <LocalPhoneRoundedIcon sx={{ color: "#06D6A0" }} fontSize="large" />
+                                    </IconButton>
+                                </a>
+                            </div>
+                            <Divider variant="middle" />
+                            <div style={{ ...contactChildrens, }}>
+                                <div style={{ maxWidth: "80%" }}>
+                                    {/* info adress */}
+                                    <h4 style={{ color: "#606060", fontWeight: "normal" }}>Address</h4>
+                                    <span style={{ color: "black" }}>
+                                        Talat N'Yaaqoub Ijoukak, 42155 Ijjoukak, Morocco</span>
+                                </div>
+                                <a href="#">
+                                    <IconButton>
+                                        <MapRoundedIcon sx={{ color: "#06D6A0" }} fontSize="large" />
+                                    </IconButton>
+                                </a>
+                            </div>
+                            <Divider variant="middle" />
+                            <div style={{ ...contactChildrens, }}>
+                                <div>
+                                    {/* info email */}
+                                    <h4 style={{ color: "#606060", fontWeight: "normal" }}>Email</h4>
+                                    <span style={{ color: "black" }}>ChezImnir@gmail.com</span>
+                                </div>
+                                <a href="#">
+                                    <IconButton>
+                                        <AttachEmailRoundedIcon sx={{ color: "#06D6A0" }} fontSize="large" />
+                                    </IconButton>
+                                </a>
+                            </div>
+                        </div>
+                        <div style={{
+                            height: "300px",
+                            width: ContactBreakingPoint ? "100%" : "60%",
+                        }}>
+                            {/* // map  */}
+                            
+
+                        </div>
                     </div>
                 </div>
             </section>
